85b599e9bb55ac88cebc9b0a9c3f72b18956789d
PavelPheonixJ <Palleast_1@mail.ru>
2022-04-25 13:14:25 +0300

    Hw3-1

---
+++ src/main/java/ru/gb/pheonix/lesson3/Lesson3.java
@@ -0,0 +1,52 @@
+package ru.gb.pheonix.lesson3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        invertArray();
+        fillArray();
+        changeArray();
+        fillDiagonal();
+    }
+
+    public static void invertArray() {
+        int[] arr = {1, 0, 1, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                System.out.print(arr[i] = 0);
+            } else {
+                System.out.print(arr[i] = 1);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void fillArray() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + i + "");
+        }
+        System.out.println();
+    }
+
+    public static void changeArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                System.out.print(" " + arr[i] * 2);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if ((arr[0]/[0] && arr[1]/[1] && arr[2]/[2] && arr[3]/[3]) == 1 ){
+                    System.out.println(1);
+                }
+            }
+        }
+    }
+
+}
