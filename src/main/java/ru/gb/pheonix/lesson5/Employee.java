d0e645ab08480033a112bba29046f5f03f70ad8a
PavelPheonixJ <Palleast_1@mail.ru>
2022-05-18 22:53:39 +0300

    HW-5

---
+++ src/main/java/ru/gb/pheonix/lesson5/Employee.java
@@ -0,0 +1,28 @@
+package ru.gb.pheonix.lesson5;
+
+// 0 Абстракция
+public class Employee {
+    private String name;
+    private String post;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+    // конструкторы
+    public Employee(String name, String post, String email, int phoneNumber, int salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+
+
+    }
+
+    public void printEmployee() {
+        System.out.println("имя: "+name + ",  должность: "+post + ",  эл.почта: "+email + ",  номер телефона: "+phoneNumber + ",  з/п в евро: "+salary + ",  возраст: "+age);
+
+    }
+}
---
+++ src/main/java/ru/gb/pheonix/lesson5/Lesson5.java
@@ -0,0 +1,24 @@
+package ru.gb.pheonix.lesson5;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+        Employee u1 = new Employee("Ivanov I.I.", "manager", "i@i.com", 589, 1500, 33); // объект (heap)
+        Employee u2 = new Employee("Петров П.П.", "security", "p@p.com", 588, 700, 25);
+        Employee u3 = new Employee("Гаврилов К.К.", "driver", "g@g.com", 587, 900, 29);
+        Employee u4 = new Employee("Якудза А.О.", "director", "j@j.com", 777, 7000, 45);
+        Employee u5 = new Employee("Метёлкина З.Р.", "secretary", "m@m.com", 776, 500, 65);
+
+        //   u1.printEmployee();
+        //   u2.printEmployee();
+        //   u3.printEmployee();
+        //   u4.printEmployee();
+        //   u5.printEmployee();
+
+
+
+    }
+
+
+
+
+}
